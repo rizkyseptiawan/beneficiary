80ce0f76e1374b3bff2283520a33ff0ad05d2d6b
rizkyseptiawan <rizseptiawan@gmail.com>
2020-06-09 16:13:53 +0700

    Feat: Api created

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Validator;
+
+class UserController extends Controller
+{
+    public function login()
+    {
+        $data= [];
+        if(auth()->attempt(['email' => request('email'), 'password' => request('password')])) {
+            $user = auth()->user();
+            $data['status'] = true;
+            $data['token'] =  $user->createToken('Breakpoin Appliction')->accessToken;
+            return response()->json($data, 200);
+        }
+        else {
+            $data['status'] = false;
+            $data['message'] = 'Unauthorized';
+            return response()->json($data, 401);
+        }
+    }
+
+    public function register(Request $request)
+    {
+        $input = $request->all();
+        $validator = Validator::make($input, [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
+        ]);
+
+        if($validator->fails()){
+            return response()->json(['status' => false , 'message' => $validator->errors()->all()], 400);
+        }
+        $user = \App\User::create([
+            'name' => $input['name'],
+            'email' => $input['email'],
+            'password' => bcrypt($input['password']),
+        ]);
+        \App\Beneficiary::create([
+            'user_id' => $user->id,
+            'nama_penerima' => $user->name,
+        ]);
+        $user->assignRole('beneficiary');
+        return response()->json(['status' => true , 'message' => 'Anda berhasil mendaftar sebagai calon penerima bantuan, silahkan login sekarang']);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,8 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+Route::post('login', 'Api\UserController@login');
+Route::post('register', 'Api\UserController@register');
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
